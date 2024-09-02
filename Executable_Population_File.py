f4b2353e756a0529abc3b4c9944dfd94ab4ffde3
Javier A Cuevas Chabrier <javy.acc@gmail.com>
2024-09-01 23:45:05 -0400

    Started making it into a executable, still not done yet

--- Executable_Population_File.py
+++ Executable_Population_File.py
@@ -2,7 +2,35 @@
 
 import tkinter as tk
 from Resources.Graph_Plotting import graph_plotting
+from Resources.Graph_Plotting import reading_files
 
 #TODO: Make this into an executable where I can click populations and see the scatter plot
+#TODO: Make buttons and entries for excel columns
 
-graph_plotting("Population (Carribean-Latin)")
+root = tk.Tk()
+root.title("Graph Plotting Calculator")
+
+window_width = 600
+window_height = 400
+
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+def init():
+    tk.Text(root, height = 8).pack
+
+def button_1():
+    reading_files()
+
+def button_2():
+    graph_plotting(r'Population_From_2019_2023.xlsx', 'Population', 'Year', 'Africa')
+
+root = root.mainloop()
